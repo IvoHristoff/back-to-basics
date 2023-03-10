12fc6b582ee1934614f85c48de1f20f2dfd02829
Ivo <hristovivo24@gmail.com>
2023-03-10 01:43:07 +0100

    created script for the clock

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
+  const clock = document.getElementById("clock");
+  setInterval(() => {
+    const seconds = new Date();
+    clock.innerHTML = seconds.toLocaleString();
+  }, 1000);
   main.style.height = `${screen.height}px`;
   window.addEventListener("resize", () => {
     main.style.height = `${screen.height}px`;
