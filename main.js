78576c3966114d0b69640003f7ea0f3ad8906c80
Ivo <hristovivo24@gmail.com>
2023-03-11 01:17:07 +0100

    reverted the way of counting time

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
   const clock = document.getElementById("clock");
-  const time = new Date();
+
   setInterval(() => {
-    time.setSeconds(time.getSeconds() + 1);
+    const time = new Date();
     clock.innerHTML = time.toLocaleTimeString([], { hour12: true });
   }, 1000);
   main.style.height = `${screen.height}px`;
