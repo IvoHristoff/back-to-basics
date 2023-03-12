1813288a80a569744565dcc9431e160639c63769
Ivo <hristovivo24@gmail.com>
2023-03-12 16:57:30 +0100

    added temporary instructions for menu

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ window.addEventListener("DOMContentLoaded", () => {
   const colorPicker = document.getElementById("color-picker");
   const textToChange = document.getElementById("text-to-change");
   const fontSizeInput = document.getElementById("font-size");
+  const footer = document.querySelector("footer");
 
   clock.style.fontSize = fontSizeInput.value + "px";
 
   const time = new Date();
   clock.innerHTML = time.toLocaleTimeString([], { hour12: true });
+
+  const showMessage = document.querySelector(".instruction-message");
 
   // running logic
   setInterval(() => {
@@ -17,6 +20,15 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 1000);
 
   // event handlers
+  footer.addEventListener("mouseover", () => {
+    footer.style.opacity = 1;
+    showMessage.style.opacity = 0;
+  });
+
+  footer.addEventListener("mouseleave", () => {
+    footer.style.opacity = 0;
+  });
+
   colorPicker.addEventListener("input", () => {
     const color = colorPicker.value;
     textToChange.style.color = color;
